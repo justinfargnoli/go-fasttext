c9bffe93e9c834512422679d7da315b5dc5d8528
Justin Fargnoli <justinfargnoli@gmail.com>
2020-07-12 16:07:44 -0400

    Splitting up testing function

--- fasttext_test.go
+++ fasttext_test.go
@@ -7,23 +7,34 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func Test_BuildDB_and_GetEmb_and_GetAllEmb(t *testing.T) {
-	ft := NewFastText(":memory:")
-	defer ft.Close()
+func build(t *testing.T) *FastText {
+	ft := New(":memory:")
 
 	file, err := os.Open("./testdata/wiki.en.vec")
-	if err != nil {
-		t.Error(err)
-	}
 	defer file.Close()
-	err = ft.BuildDB(file)
 	if err != nil {
 		t.Error(err)
 	}
+
+	err = ft.Build(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return ft
+}
+
+func TestBuild(t *testing.T) {
+	build(t)
+}
+
+func TestEmbeddingVector(t *testing.T) {
+	ft := build(t)
+	defer ft.Close()
 
 	words := []string{"has", "but", "page", "#"}
 	for _, word := range words {
-		emb, err := ft.GetEmb(word)
+		emb, err := ft.EmbeddingVector(word)
 		if err != nil {
 			t.Error(err)
 		}
@@ -32,13 +43,41 @@ func Test_BuildDB_and_GetEmb_and_GetAllEmb(t *testing.T) {
 
 	notExist := []string{"NotExist1", "Happiness"}
 	for _, word := range notExist {
-		_, err := ft.GetEmb(word)
+		_, err := ft.EmbeddingVector(word)
 		if err != ErrNoEmbFound {
 			t.Error("Should return not found")
 		}
 	}
+}
 
-	if _, err := ft.GetAllEmb(); err != nil {
+func TestAllEmbeddingVectors(t *testing.T) {
+	ft := build(t)
+	defer ft.Close()
+
+	if _, err := ft.AllEmbeddingVectors(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMultiWordEmbeddingVector(t *testing.T) {
+	ft := build(t)
+	defer ft.Close()
+
+	if _, err := ft.MultiWordEmbeddingVector([]string{"to", "be", "or", "not", "to", "be"}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMostSimilarEmbeddingVector(t *testing.T) {
+	ft := build(t)
+	defer ft.Close()
+
+	embeddingVector, err := ft.EmbeddingVector("has")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if _, _, err := ft.MostSimilarEmbeddingVector(embeddingVector); err != nil {
 		t.Error(err)
 	}
 }
